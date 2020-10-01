ca3caa48718d3f52099a7f4e0b588be6f445af69
Victoria Vosipova <crank-line@rambler.ru>
2020-10-01 19:43:39 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-
-exports.min = function min (array) {
+exports.min = function min(array) {
+  if (!array || array.length === 0) {
+    return 0;
+  }
+  array.sort((a, b) => a - b);
+  return array[0];
   return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (!array || array.length === 0) {
+    return 0;
+  }
+  array.sort((a, b) => b - a);
+  return array[0];
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (!array || array.length === 0) {
+    return 0;
+  }
+  const sum = array.reduce((total, amount) => total + amount);
+  const avg = sum / array.length;
+  return avg;
 }
